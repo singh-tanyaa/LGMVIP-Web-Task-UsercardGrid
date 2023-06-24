ab6a6bc7e02cb2d3b8c27b5c108f9293aa3fbde7
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2023-06-24 23:50:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import logo from './lgmlogo.jpg'
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://reqres.in/api/users?page=${page}`);
+      const data = await response.json();
+      setUsers(data.data);
+      setLoading(false);
+    } catch (error) {
+      console.log('Error:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+    getUsers();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="navbar">
+        <img src={logo} alt="Logo" className='logo' />
+          <div className="brand">Brand Name</div> 
+          <button className="get-users-btn" onClick={getUsers}>Get Users</button>
+        </div>
+      </nav>
+      {loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <>
+          <div className="user-card-grid">
+            {users.map((user) => (
+              <div className="user-card" key={user.id}>
+                <img src={user.avatar} alt={user.first_name} />
+                <h3>{`${user.first_name} ${user.last_name}`}</h3>
+                <p>{user.email}</p>
+              </div>
+            ))}
+          </div>
+          <button className="next-page-btn" onClick={handleNextPage}>Next Page</button>
+        </>
+      )}
     </div>
   );
 }
